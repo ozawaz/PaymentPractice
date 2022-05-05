c19035c6f39e86e3d5b34b781fd939882c0d9648
ozawa <ozawaz@163.com>
2022-05-05 21:20:03 +0800

    微信项目--API字典和相关工具

---
+++ weixin/src/main/java/cn/ozawaz/weixin/util/OrderNoUtils.java
@@ -0,0 +1,45 @@
+package cn.ozawaz.weixin.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * 订单号工具类
+ *
+ * @author ozawa
+ * @since 1.0
+ */
+public class OrderNoUtils {
+
+    /**
+     * 获取订单编号
+     * @return 返回订单编号
+     */
+    public static String getOrderNo() {
+        return "ORDER_" + getNo();
+    }
+
+    /**
+     * 获取退款单编号
+     * @return 返回退款单编号
+     */
+    public static String getRefundNo() {
+        return "REFUND_" + getNo();
+    }
+
+    /**
+     * 获取编号
+     * @return 返回编号
+     */
+    public static String getNo() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        String newDate = sdf.format(new Date());
+        StringBuilder result = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            result.append(random.nextInt(10));
+        }
+        return newDate + result;
+    }
+}
