57d530e26f633a934ae33a57595e4616a52f454a
ozawa <ozawaz@163.com>
2022-05-05 21:08:00 +0800

    微信项目--获取证书管理器实例和签名验证器和HttpClient

--- weixin/src/main/java/cn/ozawaz/weixin/config/WxPayConfig.java
+++ weixin/src/main/java/cn/ozawaz/weixin/config/WxPayConfig.java
@@ -1,14 +1,27 @@
 package cn.ozawaz.weixin.config;
 
+import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
+import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
+import com.wechat.pay.contrib.apache.httpclient.auth.Verifier;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Validator;
+import com.wechat.pay.contrib.apache.httpclient.cert.CertificatesManager;
+import com.wechat.pay.contrib.apache.httpclient.exception.HttpCodeException;
+import com.wechat.pay.contrib.apache.httpclient.exception.NotFoundException;
 import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 
 /**
@@ -25,25 +38,39 @@ import java.security.PrivateKey;
 @ConfigurationProperties(prefix="wxpay")
 public class WxPayConfig {
 
-    // 商户号
+    /**
+     * 商户号
+     */
     private String mchId;
 
-    // 商户API证书序列号
+    /**
+     * 商户API证书序列号
+     */
     private String mchSerialNo;
 
-    // 商户私钥文件
+    /**
+     * 商户私钥文件位置
+     */
     private String privateKeyPath;
 
-    // APIv3密钥
+    /**
+     * APIv3密钥
+     */
     private String apiV3Key;
 
-    // APPID
+    /**
+     * APPID
+     */
     private String appid;
 
-    // 微信服务器地址
+    /**
+     * 微信服务器地址
+     */
     private String domain;
 
-    // 接收结果通知地址
+    /**
+     * 接收结果通知地址
+     */
     private String notifyDomain;
 
     /**
@@ -57,6 +84,61 @@ public class WxPayConfig {
         } catch (FileNotFoundException e) {
             throw new RuntimeException("私钥文件不存在", e);
         }
+    }
+
+    /**
+     * 获取证书管理器实例
+     * @return 返回证书管理器实例
+     */
+    @Bean
+    public CertificatesManager getCertificatesManager() {
+        // 获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+        // 获取秘钥签名对象
+        PrivateKeySigner privateKeySigner = new PrivateKeySigner(mchSerialNo, privateKey);
+        // 身份认证对象
+        WechatPay2Credentials wechatPay2Credentials = new WechatPay2Credentials(mchId, privateKeySigner);
+        // 获取证书管理器实例
+        CertificatesManager certificatesManager = CertificatesManager.getInstance();
+        // 向证书管理器增加需要自动更新平台证书的商户信息
+        try {
+            certificatesManager.putMerchant(mchId, wechatPay2Credentials, apiV3Key.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException | GeneralSecurityException | HttpCodeException e) {
+            e.printStackTrace();
+        }
+        // ... 若有多个商户号，可继续调用putMerchant添加商户信息
+        return certificatesManager;
+    }
+
+    /**
+     * 获取签名验证器
+     * @return 返回签名验证器
+     */
+    @Bean
+    public Verifier getVerifier(CertificatesManager certificatesManager){
+        try {
+            return certificatesManager.getVerifier(mchId);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取 CloseableHttpClient 对象
+     * @return 返回CloseableHttpClient 对象
+     */
+    @Bean
+    public CloseableHttpClient getWxPayClient(Verifier verifier) {
+        // 获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+        // 获取构造器
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
+                .withMerchant(mchId, mchSerialNo, privateKey)
+                .withValidator(new WechatPay2Validator(verifier));
+        // ... 接下来，你仍然可以通过builder设置各种参数，来配置你的HttpClient
+        // 通过WechatPayHttpClientBuilder构造的HttpClient，会自动的处理签名和验签，并进行证书自动更新
+        return builder.build();
     }
 
 }
