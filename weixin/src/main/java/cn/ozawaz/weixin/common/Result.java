2e4b1c685fdb679e4a447affe6fb9918d7ce76d9
ozawa <ozawaz@163.com>
2022-05-05 15:48:21 +0800

    微信项目--统一返回结果

---
+++ weixin/src/main/java/cn/ozawaz/weixin/common/Result.java
@@ -0,0 +1,42 @@
+package cn.ozawaz.weixin.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ozawa
+ * @version 1.0
+ * @date 2022
+ * @description cn.ozawaz.weixin.common
+ * @since JDK1.8
+ */
+@Getter
+@Setter
+public class Result {
+
+    private Integer code;
+    private String message;
+    private Map<String, Object> data = new HashMap<>();
+
+    public static Result ok(){
+        Result r = new Result();
+        r.setCode(0);
+        r.setMessage("成功");
+        return r;
+    }
+
+    public static Result error(){
+        Result r = new Result();
+        r.setCode(-1);
+        r.setMessage("失败");
+        return r;
+    }
+
+    public Result data(String key, Object value){
+        this.data.put(key, value);
+        return this;
+    }
+}
--- weixin/src/main/java/cn/ozawaz/weixin/controller/PayController.java
+++ weixin/src/main/java/cn/ozawaz/weixin/controller/PayController.java
@@ -1,11 +1,14 @@
 package cn.ozawaz.weixin.controller;
 
+import cn.ozawaz.weixin.common.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author ozawa
@@ -22,8 +25,10 @@ public class PayController {
 
     @ApiOperation("测试接口")
     @GetMapping("/test")
-    public String test(){
-        return "hello";
+    public Result test(){
+        return Result.ok()
+                .data("message", "hello")
+                .data("now", new Date());
     }
 
 }
