239f45f600ae3c6f908ac74080972f773203549d
ozawa <ozawaz@163.com>
2022-05-06 22:22:55 +0800

    商户定时查询本地订单

--- weixin/src/main/java/cn/ozawaz/weixin/controller/OrderInfoController.java
+++ weixin/src/main/java/cn/ozawaz/weixin/controller/OrderInfoController.java
@@ -2,12 +2,14 @@ package cn.ozawaz.weixin.controller;
 
 import cn.ozawaz.weixin.common.Result;
 import cn.ozawaz.weixin.entity.OrderInfo;
+import cn.ozawaz.weixin.enums.OrderStatus;
 import cn.ozawaz.weixin.service.OrderInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +42,17 @@ public class OrderInfoController {
         return Result.ok().data("list", list);
     }
 
+    /**
+     * 查询本地订单状态
+     */
+    @ApiOperation("查询本地订单状态")
+    @GetMapping("/query-order-status/{orderNo}")
+    public Result queryOrderStatus(@PathVariable String orderNo) {
+        OrderInfo orderInfo = orderInfoService.lambdaQuery().eq(OrderInfo::getOrderNo, orderNo).one();
+        String orderStatus = orderInfo.getOrderStatus();
+        if (OrderStatus.SUCCESS.getType().equals(orderStatus)) {
+            return Result.ok();
+        }
+        return Result.ok().setCode(101).setMessage("支付中...");
+    }
 }
